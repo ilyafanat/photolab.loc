7b1b264f5ccf9474667899a0dd094f684a4ee888
Ilya.R <ilya.r@cudev.org>
2017-03-17 18:27:21 +0200

    initial commit

---
+++ config/development.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'application' => [
+        'debug' => true,
+        'key' => 'gB2tuMsO33'
+    ],
+    'path' => [
+        'tmp' => __DIR__ . '/../tmp/',
+        'views' => __DIR__ . '/../views/',
+        'logs' => __DIR__ . '/../logs/'
+    ]
+];
---
+++ public/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+
+$app = new Application();
+
+$app['config'] = require_once __DIR__ . '/../config/' . $app['environment'] . '.php';
+
+$app->register(new TwigServiceProvider(), [
+    'twig.path' => $app['config']['path']['views']
+]);
+
+$app->register(new MonologServiceProvider(), [
+    'monolog.logfile' => $app['config']['path']['logs'] . $context . '.log',
+    'monolog.level' => Logger::WARNING,
+    'monolog.permission' => 0777
+]);
+
+$app['debug'] = $app['config']['application']['debug'];
+
+$app->register(new ServiceControllerServiceProvider());
+
+return $app;
